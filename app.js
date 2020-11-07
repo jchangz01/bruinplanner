557da86129608adbb363dd278038880f41200599
Justin Kyle Chang <justinklchang@yahoo.com>
2020-11-06 23:25:06 -0800

    Retrieved and filtered data from devX courses API

--- app.js
+++ app.js
@@ -1,20 +1,41 @@
 const express = require('express')
+const { reset } = require('nodemon')
 var request = require('request')
 const app = express()
 const port = 8080
+
+const majorToCourse = new Map();
+const courseToCourseNum = new Map();
+
+//Retrieve and filter API data to courseNames, courseNumbers, and courseMajors
+request("http://api.ucladevx.com/courses", function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+        var parsedBody = JSON.parse(body);
+        var removeDuplicates = new Set();
+
+        for (var i = 0; i < parsedBody.length; i++)
+        {
+            if (!courseToCourseNum.get(parsedBody[i]["courseTitle"]))
+            {
+                let generalCourseNum = parsedBody[i]["courseNum"].replace(/-.*/g, '')
+
+                courseToCourseNum.set(parsedBody[i]["courseTitle"], generalCourseNum)
+
+                if (majorToCourse.get(parsedBody[i]["major"]))
+                    majorToCourse.get(parsedBody[i]["major"]).push(parsedBody[i]["courseTitle"])
+                else    
+                    majorToCourse.set(parsedBody[i]["major"], [parsedBody[i]["courseTitle"]])
+            }
+        }
+    }
+})
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
 app.get('/course', (req, res) => {
-    request("http://api.ucladevx.com/courses", function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            var parsedBody = JSON.parse(body);
-            var course1 = parsedBody[1]["courseTitle"]
-            res.send (course1); //print course1 - Introduction to Computer Science I
-        }
-    })
+    res.send(majorToCourse.get("COM SCI"))
 })
 
 app.listen(port, () => {
