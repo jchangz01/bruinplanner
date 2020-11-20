e16393a3e2526445a52c430751b0bb379ade8dfd
Justin Kyle Chang <justinklchang@yahoo.com>
2020-11-19 16:54:28 -0800

    Added styling for material-ui textfields on login page

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -2,11 +2,24 @@ import React from 'react'
 import '../css/Login.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCalendar, faArrowAltCircleLeft } from '@fortawesome/free-solid-svg-icons'
-import TextField from '@material-ui/core/TextField';
+import {createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import TextField from "@material-ui/core/TextField";
+
+
+const theme = createMuiTheme({
+palette: {
+    primary: {
+    main: "#fff",
+    },
+},
+});
+
 
 function OptionsOrPrompt (props) {
     const [form, toggleForm] = React.useState(false);
     const onClick = () => toggleForm(!form);
+
+
     return (
         <section style={{backgroundColor: "#2774AE", minheight: "100vh"}}>
             <div id="login-main-content">
@@ -16,16 +29,23 @@ function OptionsOrPrompt (props) {
                 <h1 id="Login-title">BruinPlanner</h1>
                 {form ? 
                     <React.Fragment>
-                        <TextField className="login-inputs" label="Email" />
-                        <TextField className="login-inputs" margin="normal" label="Password" />
-                        <div id="login-prompt-buttons">
-                            <span class="Login-back" onClick={onClick}>
-                                <FontAwesomeIcon className="Login-back-arrow" icon={faArrowAltCircleLeft}/>
-                                <p className="Login-back-phrase">Back</p>
-                            </span>
-                            <button className="login-button lightblue-br white" style={{float: "right", verticalAlign: "center", width: "160px"}}>Log-In</button>
-                        </div>
-                    </React.Fragment>:
+                        <form style={{width: "320px"}}>
+                            {/* Use ThemeProvider to provide desired color for your Materials-UI elements */}
+                            <ThemeProvider
+                                theme={theme}
+                            >
+                                <TextField className="login-inputs" inputProps={{ style: { fontFamily: 'Nunito', color: 'white'}}} label="Email" color="primary"/>
+                                <TextField className="login-inputs" inputProps={{ style: { fontFamily: 'Nunito', color: 'white'}}} margin="normal" label="Password" color="primary" />
+                            </ThemeProvider>
+                            <div id="login-prompt-buttons">
+                                <span class="Login-back" onClick={onClick}>
+                                    <FontAwesomeIcon className="Login-back-arrow" icon={faArrowAltCircleLeft}/>
+                                    <p className="Login-back-phrase">Back</p>
+                                </span>
+                                <button className="login-button lightblue-br white" style={{float: "right", verticalAlign: "center", width: "160px"}}>Log-In</button>
+                            </div>
+                        </form>
+                    </React.Fragment> :
                     <React.Fragment>    
                         <div id="login-buttons-container">
                             <a href="#"><button class="login-button lightblue-br white" style={{float: "left"}} onClick={onClick}>Log-in with email</button></a>
