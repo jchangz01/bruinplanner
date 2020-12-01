dda856291555bba03873d9ac8a9bdbefa4860c51
Nilay Shah <nilay.shah19@gmail.com>
2020-12-01 02:40:08 -0800

    added basic contact page

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -1,9 +1,30 @@
 import React from 'react'
-import '../css/About.css'
+import '../css/Contact.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCalendar } from '@fortawesome/free-solid-svg-icons'
+import {createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import TextField from "@material-ui/core/TextField";
 
-export default class About extends React.Component {
+
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+        main: "#000",
+        },
+    },
+}); 
+
+export default class Contact extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          firstName: '',
+          lastName:'',
+          email: '',
+          subject:'',
+          message: ''
+        }
+      }
     
     render() {
         return (
@@ -20,8 +41,101 @@ export default class About extends React.Component {
                         </nav>
                     </div>
                 </header>
+                <section>
+                    <div id="signup-main-content">
+                        <h2 id="signup-main-heading">Contact Us</h2>
+                        <h3 id="signup-main-subheading">Start planning your next 4 years at UCLA now!</h3>
+                        {/*<form action="/sign-up" method="POST" style={{width: "500px"}}> */}
+                        <form onSubmit={this.handleSubmit} style={{width: "500px"}}>  
+                            {/* Use ThemeProvider to provide desired color for your Materials-UI elements */}
+                            <ThemeProvider theme={theme}>
+                                <TextField 
+                                    onChange={event => this.setState({firstName: event.target.value})} 
+                                    value={this.state.firstName}
+                                    name="firstName" 
+                                    type="text"
+                                    className="contact-name-inputs" 
+                                    label="FirstName"
+                                    inputProps={{ style: { fontFamily: 'Nunito', color: 'black'}}}
+                                    margin="normal"
+                                    color="primary"
+                                    variant="outlined"
+                                    required/>
+                                <TextField 
+                                    onChange={event => this.setState({lastName: event.target.value})} 
+                                    value={this.state.lastName}
+                                    name="lastName" 
+                                    type="text"
+                                    className="contact-name-inputs" 
+                                    label="LastName"
+                                    inputProps={{ style: { fontFamily: 'Nunito', color: 'black'}}}
+                                    margin="normal"
+                                    color="primary"
+                                    variant="outlined"
+                                    required/>
+                                <TextField 
+                                    onChange={event => this.setState({email: event.target.value})} 
+                                    value={this.state.email}
+                                    name="email"
+                                    type="email" 
+                                    className="contact-inputs" 
+                                    label="Email" 
+                                    inputProps={{ style: { fontFamily: 'Nunito', color: 'black'}}} 
+                                    margin="normal"  
+                                    color="primary" 
+                                    variant="outlined"
+                                    required/>
+                                <TextField 
+                                    onChange={event => this.setState({subject: event.target.value})} 
+                                    value={this.state.subject} 
+                                    name="subject" 
+                                    type="text" 
+                                    className="contact-inputs" 
+                                    label="Subject" 
+                                    inputProps={{ style: { fontFamily: 'Nunito', color: 'black'}}} 
+                                    margin="normal"  
+                                    color="primary" 
+                                    variant="outlined"
+                                    required/>
+                                <TextField 
+                                    onChange={event => this.setState({message: event.target.value})} 
+                                    value={this.state.message} 
+                                    name="message" 
+                                    type="text" 
+                                    className="contact-inputs" 
+                                    label="Message" 
+                                    rows={8}
+                                    multiline
+                                    inputProps={{ style: { fontFamily: 'Nunito', color: 'black'}}} 
+                                    margin="normal" 
+                                    color="primary" 
+                                    variant="outlined"
+                                    required/>
+                            </ThemeProvider>
+                            <button type="submit" className="logSign-button blue-br white" style={{float: "right", verticalAlign: "center", width: "250px", marginTop:"30px"}}>Create-account</button>
+                        </form>
+                    </div>
+                </section>
             </div>
+                
         )
     }
+    onNameChange(event) {
+        this.setState({name: event.target.value})
+      }
+    
+      onEmailChange(event) {
+        this.setState({email: event.target.value})
+      }
+    
+      onMessageChange(event) {
+        this.setState({message: event.target.value})
+      }
+    
+      handleSubmit(event) {
+      }
 
 }
+
+
+ 
